18dab01f704a8e41a3094aa531b877303009a461
Mayur Rangnath Suryawanshi <surywanshi.mr@gmail.com>
2025-12-24 10:26:45 +0530

    Bugs fixed
    Create Order and Checklist creation added. Bug fixed

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-    clientId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    clientId: {
+  type: String,
+  required: true
+},
+
     procurementManagerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     inspectionManagerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     checklistId: { type: mongoose.Schema.Types.ObjectId, ref: "Checklist" },
